f0276678fc8afb4c7b2a31f1933c824ccaf645ae
Ifeoma Jonah <ifyajoke@gmail.com>
2020-07-30 15:38:40 -0700

    Vue First Task

---
+++ src/components/MyName.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="introduce">
+    <h1>{{ name }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color:rgb(235, 26, 123);
+  font-size: 45px;
+}
+</style>
